14cded4c61e1122ffa67b78bd64e26d29200d904
aayush-tiw <aceaayush6@gmail.com>
2026-09-22 00:11:07 +0530

    Add Week 6 Day 1 Playwright practice

---
+++ Week-6/Day1/playwright/tests/example.spec.js
@@ -0,0 +1,7 @@
+const { test, expect } = require('@playwright/test');
+
+test('Day 5 API page loads', async ({ page }) => {
+  await page.goto('http://host.docker.internal:8001');
+
+  await expect(page.locator('body')).toContainText('Hello from Environment');
+});
